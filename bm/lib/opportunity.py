551f172517b91f2939e8ff15a008ec66ee8e105c
Janakiraman Ganesan <jana.ganesan@tradingtechnologies.com>
2019-07-04 14:52:11 +0900

    Change trade bucket to 48h.

--- bm/lib/opportunity.py
+++ bm/lib/opportunity.py
@@ -142,7 +142,7 @@ class Opportunity:
         while True:
             trade_bucket = None
             while trade_bucket is None:
-                past_xh = datetime.utcnow() - timedelta(hours=24)
+                past_xh = datetime.utcnow() - timedelta(hours=48)
                 trade_bucket = self.client.trade_bucket(binSize="1h", startTime=past_xh)
 
             tide = []
